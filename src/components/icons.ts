fb145f31ee7ff2d0d12c51873ccdf4a34120961a
Maria Ek <maria.eken@gmail.com>
2025-09-08 13:42:20 +0200

    importerar icon ist f kopiera. Lagt till källan så vi kan hitta fler ikoner vid behov.

---
+++ src/components/icons.ts
@@ -0,0 +1,9 @@
+// Ändrade här till att importera ikoner istället för att kopiera. Då kan vi fylla på med fler ikoner här vid behov. 
+
+// Alla ikoner hämtade från Material UI Icons:
+// https://mui.com/material-ui/material-icons/
+
+
+export { default as GroupIcon } from "@mui/icons-material/Group"; // "group" (personer)
+
+
