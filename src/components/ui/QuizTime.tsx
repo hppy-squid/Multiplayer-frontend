6d3f5bb0b09a706708d697e401081a90ec580060
Felicia Karlsson <felizia02@icloud.com>
2025-09-09 12:42:24 +0200

    Quiz sida byggd

---
+++ src/components/ui/QuizTime.tsx
@@ -0,0 +1,9 @@
+export function QuizTime() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+        <h2 className="text-3xl font-bold text-gray-800 mb-4">15 sek</h2>
+        </div>
+  )
+}
+
+      
